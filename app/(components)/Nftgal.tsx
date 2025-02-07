e780f9caf21ee3bff2233d59f8659c17c5208d43
adamaslan <chillcoders@gmail.com>
2025-02-07 18:02:58 -0500

    nft-two-comp-syst

--- app/(components)/Nftgal.tsx
+++ app/(components)/Nftgal.tsx
@@ -1,23 +1,33 @@
-"use client";
-
-import NFTCard, { NFTItem } from './NFTCard';
-
-interface NFTGalleryProps {
-  nfts: NFTItem[];
-}
-
-const NFTGallery: React.FC<NFTGalleryProps> = ({ nfts }) => {
+const NFTGallery = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
-      {nfts.map((nft, index) => (
-        <NFTCard
-          key={index}
-          contractAddress={nft.contractAddress}
-          tokenId={nft.tokenId}
-        />
-      ))}
+      <p>NFT Gallery</p>
     </div>
   );
 };
 
+
 export default NFTGallery;
+// "use client";
+
+// import NFTCard, { NFTItem } from './NFTCard';
+
+// interface NFTGalleryProps {
+//   nfts: NFTItem[];
+// }
+
+// const NFTGallery: React.FC<NFTGalleryProps> = ({ nfts }) => {
+//   return (
+//     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+//       {nfts.map((nft, index) => (
+//         <NFTCard
+//           key={index}
+//           contractAddress={nft.contractAddress}
+//           tokenId={nft.tokenId}
+//         />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default NFTGallery;
